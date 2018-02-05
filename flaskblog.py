97318b365184a97cb114e09ce964577a2a47a1ea
Muhamad Fakhrusy <mfakhrusy@outlook.com>
2018-02-05 16:11:40 +0700

    add npm things

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,6 @@
 from app import app, db
 from app.models import User, Post
 
-@app.shell_context_processor
+@app.shell_context_processor #  this fungtion is to make `flask shell` work
 def make_shell_context():
     return {'db': db, 'User': User, 'Post': Post}
